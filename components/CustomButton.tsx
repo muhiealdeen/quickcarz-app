f07890643d542984302f9bdb98bcf1e3eff94def
Muhie Aldeen <zezo1998@hotmail.com>
2024-08-28 10:54:47 +0200

    I created a custombuttom file and a folder for types and index file inside it

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+'use client';
+// components/CustomButton.tsx"
+import React from 'react';
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      <span className="flex-1">title</span>
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+// components/index.ts
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+
+export { Hero, CustomButton };
