ba380bb3dbda1e35b452327d890b99efb935faa9
Muhie Aldeen <zezo1998@hotmail.com>
2024-08-30 21:44:22 +0200

    I created the navbar and footer files

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 // components/index.ts
 import Hero from './Hero';
 import CustomButton from './CustomButton';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, NavBar, Footer };
