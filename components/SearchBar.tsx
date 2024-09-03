de544a479e2e808670d6b840503cc3007573af87
Muhie Aldeen <zezo1998@hotmail.com>
2024-09-03 15:25:37 +0200

    I am  developing the SearchBar component I installed the headless ui and started use combbox

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,19 @@
 'use client';
 import React from 'react';
+import { SeachManufacturer } from './';
+import { useState } from 'react';
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState('');
   const handleSearch = () => {};
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item">SearchBar</div>
+      <div className="searchbar__item">
+        <SeachManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
     </form>
   );
 };
